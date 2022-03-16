884cc39aefd1570c360a8f92bc721ae6e859bf12
Maria <salyginamaria156@gmail.com>
2022-03-16 22:51:02 +0300

    Lab7

--- Timer/main.js
+++ Timer/main.js
@@ -5,18 +5,43 @@ let div = document.querySelector(".num");
 
 //Задание 1
 
-let intervalId = setInterval(()=>{
-    if ( parseInt(div.textContent)> 0){
-        div.textContent = parseInt(div.textContent) - 1;
-    };
-}, 1000); 
+// let intervalId = setInterval(()=>{
+//     if ( parseInt(div.textContent)> 0){
+//         div.textContent = parseInt(div.textContent) - 1;
+//     };
+// }, 1000); 
 
 
-btnStart.addEventListener("click", ()=>{
-    div.textContent = input.value;
-    intervalId;
-})
+// btnStart.addEventListener("click", ()=>{
+//     div.textContent = input.value;
+//     intervalId;
+// })
 
-btnStop.addEventListener("click", ()=>{
-    clearInterval(intervalId);
-})
+// btnStop.addEventListener("click", ()=>{
+//     clearInterval(intervalId);
+// })
+
+
+//Задание 2
+let inp = document.createElement("input");
+document.body.append(inp);
+
+let h2 = document.createElement("h2");
+document.body.append(h2);
+
+let inpValue  = inp.value;
+
+let Sec = setTimeout (function Now(){
+
+    if (inpValue != inp.value){
+        clearTimeout(Sec);
+        inpValue = inp.value;
+        setTimeout(Now, 1200);
+    }
+    else{
+        setTimeout(()=>{
+            h2.textContent = inpValue;
+        } ,300)
+        
+    }
+}, 4000);
